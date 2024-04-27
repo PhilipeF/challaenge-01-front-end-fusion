9b963db0a307f5579af79017443aca187684bdf4
PhilipeF <philipesferreiraa@gmail.com>
2024-04-27 11:40:45 -0300

    Organizando melhor o projeto em pastas

--- src/js/script.js
+++ src/js/script.js
@@ -2,7 +2,7 @@ const btnMenuMobile = document.getElementById('btn-menu-mobile');
 const menuMobile = document.getElementById('menu-mobile');
 menuMobile.addEventListener('click', animar);
 
-function animar() {
+function animarMenuMobile() {
     menuMobile.classList.toggle('abrir')
     btnMenuMobile.classList.toggle('ativar')    
 }
